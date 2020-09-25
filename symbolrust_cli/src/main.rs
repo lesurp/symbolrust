8d1ed8652932b93132100bfb3b8f1f4f82f4da18
Paul Lesur <git@lesurpaul.fr>
2020-09-25 18:58:42 +0200

    working version of the CLI!

--- symbolrust_cli/src/main.rs
+++ symbolrust_cli/src/main.rs
@@ -1,6 +1,5 @@
 use lazy_static::lazy_static;
 use pest::{
-    error::Error,
     iterators::Pair,
     iterators::Pairs,
     prec_climber::{Assoc, Operator, PrecClimber},
@@ -8,14 +7,46 @@ use pest::{
 };
 use pest_derive::Parser;
 use std::collections::{hash_map::Entry, HashMap};
+use std::io::{BufRead, Write};
 use symbolrust_core::prelude::*;
+use symbolrust_core::visitors::PrettyPrinterContext;
 
 #[derive(Parser)]
 #[grammar = "../grammar.pest"] // relative to src
 struct MyParser;
 
+#[derive(Default)]
+struct VariableMap {
+    v2n: PrettyPrinterContext,
+    n2v: HashMap<String, Variable>,
+}
+
+impl VariableMap {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn name(&mut self, n: String) -> Variable {
+        match self.n2v.entry(n) {
+            Entry::Vacant(vac) => {
+                let var = Variable::new();
+                self.v2n.name_var(var, vac.key().clone());
+                *vac.insert(var)
+            }
+            Entry::Occupied(occ) => *occ.get(),
+        }
+    }
+
+    pub fn var(&mut self, v: Variable) -> Option<&String> {
+        self.v2n.as_map().get(&v)
+    }
+
+    pub fn as_context(&self) -> &PrettyPrinterContext {
+        &self.v2n
+    }
+}
+
 struct AstBuilder {
-    parse_context: HashMap<String, Variable>,
     fn_to_ctor: HashMap<String, fn(Vec<Node>) -> Result<Node, ()>>,
 }
 
@@ -40,36 +71,57 @@ fn infix(lhs: Node, op: Pair<Rule>, rhs: Node) -> Node {
     }
 }
 
+enum TopLevelExpression {
+    Expr(Node),
+    Assignment(Variable, Node),
+}
+
 impl AstBuilder {
     fn new() -> Self {
         let mut fn_to_ctor = HashMap::new();
         fn_to_ctor.insert("exp".to_owned(), Exponential::from_args as _);
-        AstBuilder {
-            parse_context: HashMap::new(),
-            fn_to_ctor,
+        AstBuilder { fn_to_ctor }
+    }
+
+    pub fn build_top_level<'i>(
+        &mut self,
+        mut pair: Pairs<'i, Rule>,
+        var_map: &mut VariableMap,
+    ) -> TopLevelExpression {
+        let pair = pair.next().unwrap();
+        match pair.as_rule() {
+            Rule::assignment => {
+                let mut it = pair.into_inner();
+                let var_name = it.next().unwrap().as_str();
+                let expr = it.next().unwrap();
+                let node = self.build_expr(expr.into_inner(), var_map);
+                let var = var_map.name(var_name.to_owned());
+
+                TopLevelExpression::Assignment(var, node)
+            }
+            Rule::expr => TopLevelExpression::Expr(self.build_expr(pair.into_inner(), var_map)),
+            _ => unreachable!(),
         }
     }
 
-    fn build_expr<'i>(&mut self, pair: Pairs<'i, Rule>) -> Node {
-        PREC_CLIMBER.climb(pair, |p| self.build_subexpr(p), infix)
+    fn build_expr<'i>(&mut self, pair: Pairs<'i, Rule>, var_map: &mut VariableMap) -> Node {
+        PREC_CLIMBER.climb(pair, |p| self.build_subexpr(p, var_map), infix)
     }
 
-    fn build_subexpr<'i>(&mut self, pair: Pair<'i, Rule>) -> Node {
+    fn build_subexpr<'i>(&mut self, pair: Pair<'i, Rule>, var_map: &mut VariableMap) -> Node {
         match pair.as_rule() {
-            Rule::expr => self.build_expr(pair.into_inner()),
+            Rule::expr => self.build_expr(pair.into_inner(), var_map),
             Rule::constant => pair.as_str().parse::<f64>().unwrap().into(),
             Rule::var => {
                 let var_name = pair.as_str().to_owned();
-                match self.parse_context.entry(var_name) {
-                    Entry::Vacant(vac) => *vac.insert(Variable::new()),
-                    Entry::Occupied(occ) => *occ.get(),
-                }
-                .into()
+                var_map.name(var_name).into()
             }
             Rule::function_call => {
                 let mut it = pair.into_inner();
                 let fn_name = it.next().unwrap().as_str();
-                let args = it.map(|pair| self.build_expr(pair.into_inner())).collect();
+                let args = it
+                    .map(|pair| self.build_expr(pair.into_inner(), var_map))
+                    .collect();
                 if let Some(func) = self.fn_to_ctor.get(fn_name) {
                     func(args).unwrap()
                 } else {
@@ -77,14 +129,18 @@ impl AstBuilder {
                 }
             }
 
-            Rule::operation | Rule::int | Rule::num | Rule::WHITESPACE | Rule::calculation => {
-                unreachable!()
-            }
+            // shouldn't appear at all
+            Rule::operation
+            | Rule::identifier
+            | Rule::line
+            | Rule::term
+            | Rule::int
+            | Rule::num
+            | Rule::WHITESPACE
+            | Rule::assignment => unreachable!(),
+            // consumed by the prec_climber
             Rule::add | Rule::subtract | Rule::multiply | Rule::divide | Rule::power => {
                 unreachable!();
-            }
-            _ => {
-                todo!();
             }
         }
     }
@@ -93,9 +149,44 @@ impl AstBuilder {
 fn main() {
     let mut ast_builder = AstBuilder::new();
 
-    let pairs = MyParser::parse(Rule::calculation, " x- 3*y + exp(5*z / -2.0e2)")
-        .unwrap_or_else(|e| panic!("{}", e));
-    let result = ast_builder.build_expr(pairs);
-    let pp = PrettyPrinter::new();
-    println!("Printing parsed ast: {}", result.accept_visitor(&pp));
+    let mut var_map = VariableMap::new();
+    let mut var_context = VariableContext::new();
+
+    let stdin = std::io::stdin();
+
+    let new_line = || {
+        print!(">> ");
+        std::io::stdout().flush().unwrap();
+    };
+
+    new_line();
+    for line in stdin.lock().lines() {
+        let line = if let Ok(line) = line {
+            line
+        } else {
+            break;
+        };
+
+        let pairs = MyParser::parse(Rule::line, &line).unwrap_or_else(|e| panic!("{}", e));
+        let result = ast_builder.build_top_level(pairs, &mut var_map);
+
+        match result {
+            TopLevelExpression::Expr(expr) => {
+                let evaluated = Evaluator::evaluate(&expr, &var_context);
+                let folded = ConstantFolder::fold(&evaluated);
+                let as_str = PrettyPrinter::print_with_context(&folded, var_map.as_context());
+                println!("\t{}", as_str);
+            }
+
+            TopLevelExpression::Assignment(var, expr) => {
+                let as_str = PrettyPrinter::print_with_context(&expr, var_map.as_context());
+                let name = var_map.var(var).unwrap();
+                println!("\tassigning to: {}", name);
+                println!("\tthe value: {}", as_str);
+                var_context.insert(var, expr);
+            }
+        }
+
+        new_line();
+    }
 }
